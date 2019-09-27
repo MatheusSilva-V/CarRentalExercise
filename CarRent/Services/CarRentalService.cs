15e3f6b95c79fd83fb6b4790c04f08b9380b4bdf
Matheus Silva <matheuscs17@gmail.com>
2019-09-27 20:03:42 -0300

    Exercicio aluguel de carros, com interface

---
+++ CarRent/Services/CarRentalService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using CarRent.Entities;
+using CarRent.Services;
+
+namespace CarRent.Services
+{
+	public class CarRentalService
+	{
+		public double PricePerHour { get; private set; }
+		public double PricePerDay { get; private set; }
+		
+		public ITaxService _taxService;
+		
+		public CarRentalService (double pricePerHour, double pricePerDay, ITaxService taxService)
+		{
+			PricePerHour = pricePerHour;
+			PricePerDay = pricePerDay;
+			_taxService = taxService;
+		}
+		
+		public void ProcessInvoice (CarRental carRental)
+		{
+			TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+			double basicPayment = 0.0;
+			
+			if (duration.TotalHours <= 12.0)
+			{
+				basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+			}
+			else 
+			{
+				basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+			}
+			
+			double tax = _taxService.Tax(basicPayment);
+			
+			carRental.Invoice = new Invoice(basicPayment, tax);
+		}	
+	}
+}
---
+++ CarRent/Services/ITaxService.cs
@@ -0,0 +1,7 @@
+﻿namespace CarRent.Services
+{
+	public interface ITaxService
+	{
+		double Tax (double amount);
+	}
+}
